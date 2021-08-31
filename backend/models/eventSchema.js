0bdad19b8c42ab1c4796403b05e8bb8d5ab274ed
Max Eichhorn <m.3ichhorn@gmail.com>
2021-08-31 20:22:10 +0200

    added loanEvents

---
+++ backend/models/eventSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema
+
+const eventSchema = new schema({
+    objects: [{ object: { type: schema.Types.ObjectId } }],
+    loanPurpose: { type: String, required: true },
+    loanName: { type: String, required: true },
+    contactPerson: { type: String, required: true },
+    contactEmail: { type: String, required: true },
+    loanStartDate: { type: Date, required: true },
+    loanEndDate: { type: Date, required: true },
+    workflowState: { type: String, required: true }
+
+})
+
+module.exports = mongoose.model("event", eventSchema)
